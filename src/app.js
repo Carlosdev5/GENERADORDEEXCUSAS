b386da655d695aa8da534b9a7469d491e5d2348e
Carlosdev5 <154332876+Carlosdev5@users.noreply.github.com>
2024-08-19 15:32:14 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let h1 = document.getElementById("excuse");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let getRandomInt = array => {
+    let index = Math.floor(Math.random() * array.length);
+    return index;
+  };
+  let excuse =
+    who[getRandomInt(who)] +
+    " " +
+    action[getRandomInt(action)] +
+    " " +
+    what[getRandomInt(what)] +
+    " " +
+    when[getRandomInt(when)];
+  h1.innerHTML = excuse;
 };
